33e7f6574e623713494fd5957746c64a4d67999a
gustavo-kissoka <guguplaergustavo@gmail.com>
2026-08-14 17:36:54 +0100

    pasta config/database.php criada e index.html alterado para index.php.

---
+++ dia-056-agenda-de-contactos/config/database.php
@@ -0,0 +1,22 @@
+<?php
+function conectarBD() {
+    $host = 'localhost';
+    $db = 'agenda_de_contactos';
+    $user = 'root';
+    $pass = '';
+
+    $dsn = "mysql:host=$host;dbname=$db";
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ]);
+
+        return $pdo;
+    }catch (PDOException $e) {
+        die("Erro de Conexão com a base" . $e->getMessage());
+    }
+}
+?>
